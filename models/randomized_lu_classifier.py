1759a9d035ec27d53f36777ffb87bf050e4a31c6
Parker J. Rule <parker.rule@tufts.edu>
2021-12-13 21:35:33 -0500

    Fix bug in randomized LU classifier; standardize formatting

--- models/randomized_lu_classifier.py
+++ models/randomized_lu_classifier.py
@@ -1,25 +1,23 @@
+"""Randomized LU-based classifier."""
 import numpy as np
 from numpy.linalg import pinv, norm
 from sklearn.base import BaseEstimator, ClassifierMixin
+from randomized_lu import randomized_lu
 
-class DictLearner(BaseEstimator, ClassifierMixin):
-    
+
+class RandomizedLUClassifier(BaseEstimator, ClassifierMixin):
+    """Randomized LU-based classifier."""
     def dist(self, x: np.ndarray, D: np.ndarray) -> float:
         """Calculates the distance metric between an input
            vector x and a dictionary D
 
         Args:
             x: The input vector.
-            D: A trained dictionary.
+            D: A trained dictionary multiplied by its pseudoinverse.
 
         Returns:
             The distance between x and D."""
-#         print(D.shape)
-#         print(pinv(D).shape)
-#         print(x.shape)
-
-        
-        return norm((D @ pinv(D))[:x.shape[0],:x.shape[0]] * x - x) ## WTH - dimensions don't align
+        return norm(D @ x - x)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """Predicts which class the input vector x belongs to
@@ -30,10 +28,13 @@ class DictLearner(BaseEstimator, ClassifierMixin):
 
         Returns:
             The predicted class of each vector in X."""
-        
-        
-        class_predictions = [self.classes[np.argmin([self.dist(x, self.dictionaries[class_]) for class_ in self.classes])] 
-                             for x in X]
+
+        class_predictions = [
+            self.classes[np.argmin([
+                self.dist(x, self.dictionary_invs[class_])
+                for class_ in self.classes
+            ])] for x in X
+        ]
         return class_predictions
 
     def fit(self, X: np.ndarray, y: np.ndarray, k: dict = {}) -> list:
@@ -46,14 +47,14 @@ class DictLearner(BaseEstimator, ClassifierMixin):
 
         Returns:
             A DictLearner object with a trained dictionary for each class"""
-        
-        self.dictionaries = dict()
-        
+        self.dictionaries = {}
+        self.dictionary_invs = {}
         self.classes = np.unique(y)
+
         for class_ in self.classes:
             X_class = X[np.where(y == class_)[0]]
-            P, Q, L, U = randomized_lu(X_class, k[class_], k[class_] + 5)
-#             print(P.T.shape, L.shape)
-            self.dictionaries[class_] = P.T @ L
-            
+            P, Q, L, U = randomized_lu(X_class.T, k[class_], k[class_] + 5)
+            D = P.T @ L
+            self.dictionaries[class_] = D
+            self.dictionary_invs[class_] = D @ pinv(D)
         return self
